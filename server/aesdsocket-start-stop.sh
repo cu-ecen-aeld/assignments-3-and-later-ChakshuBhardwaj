35830298b471ad0f8bdd1371a8d753e49d9cd890
Chakshu Bhardwaj <chakshu.bhardwaj93@gmail.com>
2024-05-11 00:32:45 -0400

    Updating the script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,20 +1,27 @@
-#!/bin/sh
+#! /bin/sh
+
+# server name variable 
+#SREVER_NAME=$(echo $0 | cut -d '-' -f 1)
+# Define a variable for the current directory
+#SERVER_DIR="$PWD"
 
 case "$1" in
-start)
-    echo "Starting aesdsocket"
-    start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
-    #start-stop-daemon --start --background --exec /usr/bin/aesdsocket
-    ;;
-    
-stop)
-    echo "Stopping aesdsocket"
-    start-stop-daemon -K -n aesdsocket
-    #start-stop-daemon --stop --exec /usr/bin/aesdsocket
+    start)
+        echo "Starting aesdsocket server"
+        start-stop-daemon -S --oknodo -n aesdsock -a /usr/bin/aesdsocket -- -d --verbose
         ;;
-*)
-    echo "Usage: $0 {start|stop}"
-    exit 1
+    stop)
+        echo "Terminating aesdsocket server"
+        start-stop-daemon -K --oknodo -n aesdsocket --verbose
+        ;;
+    test)
+        echo "Testing aesdsocket server"
+        start-stop-daemon --test -S --oknodo -n aesdsocket -a /usr/bin/aesdsocket --verbose
+        ;;
+    *)
+        echo "Invalid Option!"
+        exit 1
+        ;;
 esac
 
 exit 0
