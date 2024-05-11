84f1724bd1477975140ec89fee07b51bb6da1a9c
Chakshu Bhardwaj <chakshu.bhardwaj93@gmail.com>
2024-05-10 21:14:37 -0400

    Updated ioclt char driver

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -160,8 +160,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     if (memchr(dev->entry.buffptr, '\n', dev->entry.size))
     {
 
-        const char *replaced_buffer = aesd_circular_buffer_add_entry(&(dev->circular_buffer), &(dev->entry));
-        kfree(replaced_buffer);
+        aesd_circular_buffer_add_entry(&(dev->circular_buffer), &(dev->entry));
         dev->entry.buffptr = NULL;
         dev->entry.size = 0;
     }
